be27d7f9716de9e7ffa121f44ff48f1bd52570f9
jharrison23 <Jh0698018848>
2016-12-19 15:55:43 -0500

    Initial Commit

---
+++ iris.py
@@ -0,0 +1,5 @@
+# Machine Learning Classification algorithm to identify the type of flower
+# based off of the features Sepal Length and Width, and Petal Length and Width
+# Written by James Harrison following along with a google researcher tutorial
+
+# Data set obtained from the URL https://en.wikipedia.org/wiki/Iris_flower_data_set
