27a7264f803eb442a460b69846978d42a2fa2fe3
jharrison23 <Jh0698018848>
2016-12-19 17:10:13 -0500

    Printed all of the dataset along with the labels, split the dataset into a testing set and training set, so we can train the decision tree classifier which will be used

--- iris.py
+++ iris.py
@@ -11,6 +11,10 @@
 # Iris is included in the sample datasets for scikit learn so we can import it
 from sklearn.datasets import load_iris
 
+# import decision tree classifier to be used for Classification
+from sklearn import tree
+
+import numpy as np
 # load and return the iris dataset
 # this data set includes the data from the link as well as meta data, which tells you the
 # names of the features and the names of the flowers
@@ -31,3 +35,23 @@ print iris.data[0]
 # this will display the first flower which has the features of data[0]
 # it will return an index which corresponds to the array in target_names
 print iris.target[0]
+
+# Loop through all of the dataset and print the entire table including the labels and features
+# added 1 to the i so that i said example 1 instead of example 0
+for i in range(len(iris.data)):
+    print "Example %d : label %s, features %s" % (i + 1, iris.target[i], iris.data[i])
+
+
+# Train classifier
+# first split data into testing set and training data
+# Store some elements from the dataset into the test set
+test_idx = [0, 50, 100]
+
+# remove the entries which are stored in the test set from the original data and target variables
+# this set of variables represent the training set without the entries from the testing set
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis = 0)
+
+# These variables represent the testing set
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
