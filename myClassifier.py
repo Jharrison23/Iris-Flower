eb1582bd499f93eea72708577c568012f26acede
jharrison23 <Jh0698018848>
2016-12-20 16:16:34 -0500

    imported the iris dataset and split the dataset into two partitions one for testing and one for training, same code as in the other files

---
+++ myClassifier.py
@@ -0,0 +1,17 @@
+# import iris dataset from sklearn
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+# seperate th features into the variable x and the labels into Y
+# we do this because we can think of a classifier as a function f(x) = y
+x = iris.data
+y = iris.target
+
+# Partition the dataset into a training and testing set
+from sklearn.cross_validation import train_test_split
+
+# x_train and y_train represent the features and labels for the training set
+# x_test and y_test represent the features and labels for the testing set
+# test_size is saying what percentage of the data you want for testing .5 = 50%
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .5)
